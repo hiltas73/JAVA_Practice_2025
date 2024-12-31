f310a13ca113f31272adb3924708659da9e6c5da
hiltas73 <h_iltas@yahoo.com>
2024-12-31 12:14:12 +0300

    Project created and Greater Number solved

---
+++ src/January_25/GreaterNumber.java
@@ -0,0 +1,27 @@
+package January_25;
+
+import java.util.Scanner;
+
+public class GreaterNumber {
+
+/**
+ * Use an IF statement to determine if the number a is greater than the other given number b
+ */
+
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number a");
+    int a = sc.nextInt();
+    System.out.println("Enter the second number b");
+    int b = sc.nextInt();
+
+    if (a > b){
+        System.out.println(a + " is greater than " + b);
+    }else {
+        System.out.println(b + " is greater than " + a);
+    }
+
+}
+
+}
